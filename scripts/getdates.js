3b5a1bb90ff945eeddac5e1a9791111601236481
daphineayaa <ayaadaphine0@gmail.com>
2025-05-11 10:01:02 -0700

    fix

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,9 @@
 // Set the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("year").textContent = new Date().getFullYear();
+// Set the current date
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 // Set the last modified date
-document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
+// Set the current date
+const currentDate = new Date();
